5fd453ffde309c62f7c88b722222fecd538373ce
nathancharles <nathancookster@gmail.com>
2016-01-15 13:24:58 -0800

    add test page

---
+++ adLoader.js
@@ -0,0 +1,9 @@
+'use strict';
+(function(window) {
+  var iframe = document.createElement('iframe');
+
+  // Reset src to the correct value
+  iframe.src = 'http://1.cuzillion.com/bin/resource.cgi?type=js&sleep=5&n=1&t=3529962513130.158';
+
+  document.body.appendChild(iframe);
+})(window);
