61a624f5e0c62b300ac2e7cdaa12b31890fbc82d
nathancharles <nathancookster@gmail.com>
2016-01-19 10:50:10 -0800

    add other demos

--- adLoader.js
+++ adLoader.js
@@ -1,9 +1,11 @@
 'use strict';
 (function(window) {
-  var iframe = document.createElement('iframe');
+  var iframe = document.createElement('iframe'),
+      a = Math.random() + "",
+      t = a * 10000000000000;
 
   // Reset src to the correct value
-  iframe.src = 'http://1.cuzillion.com/bin/resource.cgi?type=js&sleep=5&n=1&t=3529962513130.158';
+  iframe.src = 'http://1.cuzillion.com/bin/resource.cgi?type=js&sleep=5&n=1&t=' + t;
 
   document.body.appendChild(iframe);
 })(window);
