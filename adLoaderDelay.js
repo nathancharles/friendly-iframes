005598fc89ffb0e31e8618b917a1d00663821b2c
nathancharles <nathancookster@gmail.com>
2016-01-19 13:56:52 -0800

    add delay pages.

---
+++ adLoaderDelay.js
@@ -0,0 +1,17 @@
+'use strict';
+(function(window) {
+
+  var iframe = document.createElement('iframe'),
+      a = Math.random() + "",
+      t = a * 10000000000000,
+      timeout = parseInt(document.body.getAttribute('data-delay'), 10);
+
+  timeout = Number.isNaN(timeout) ? 0 : timeout;
+
+  // Reset src to the correct value
+  iframe.src = 'http://1.cuzillion.com/bin/resource.cgi?type=js&sleep=5&n=1&t=' + t;
+
+  setTimeout(function() {
+    document.body.appendChild(iframe);
+  }, timeout);
+})(window);
