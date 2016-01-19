1a2e642d03c4017ac81b4ca63921f47994c37dda
nathancharles <nathancookster@gmail.com>
2016-01-19 14:08:54 -0800

    update timeout to use ms

--- adLoaderDelay.js
+++ adLoaderDelay.js
@@ -6,7 +6,7 @@
       t = a * 10000000000000,
       timeout = parseInt(document.body.getAttribute('data-delay'), 10);
 
-  timeout = Number.isNaN(timeout) ? 0 : timeout;
+  timeout = (Number.isNaN(timeout) ? 0 : timeout) * 1000;
 
   // Reset src to the correct value
   iframe.src = 'http://1.cuzillion.com/bin/resource.cgi?type=js&sleep=5&n=1&t=' + t;
