8d9ceb948ea0ca5654a0a19576f562fe37808ebe
nathancharles <nathancookster@gmail.com>
2016-01-21 19:39:24 -0800

    put xhr in defer

--- adLoaderReplace.js
+++ adLoaderReplace.js
@@ -1,9 +1,21 @@
 'use strict';
 (function(window) {
+  function defer(method) {
+    if (window.jQuery) {
+      method();
+    } else {
+      setTimeout(function(){
+        defer(method);
+      }, 10);
+    }
+  }
+
   var adUrl = document.body.getAttribute('data-ad-url');
 
-  $.get(adUrl).then(function(htmlContent) {
-    document.write(htmlContent);
-    document.close();
+  defer(function() {
+    $.get(adUrl).then(function(htmlContent) {
+      document.write(htmlContent);
+      document.close();
+    });
   });
 })(window);
